e693c847897840abecc042fb09de74f5aee81b36
Marya Lebedeva <lebedeva.mk@phystech.edu>
2022-05-02 20:04:43 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# To start with
+
+Run the command line: python3 otbor.py
+
+# Functionality:
+Admins have an opportunity to add new order to database with order status
+
+# Technical details:
+
+## back-end
+In this project I use the REST-API architecture. In this API, there is an ability to add an order (/add_new_order), to change an order (/update_order), to delete an order (/delete_order). To view all orders from the database, I implemented /all_orders endpoint.
+
+## front-end
+I made a convenient window for the administrator, with which you can easily add a new order.
---
+++ otbor.py
@@ -0,0 +1,79 @@
+import sqlite3
+from flask import Flask
+
+conn = sqlite3.connect('otbor4.s3db', check_same_thread=False)
+cur = conn.cursor()
+
+
+# creating table
+#
+# cur.execute("""CREATE TABLE t1 (
+#     --name TEXT,
+#     --address TEXT,
+#     --number INT,
+#     order_id INT,
+#     --sum INT,
+#     status TEXT
+# )""")
+
+
+def add_order(cur, order_id, status):
+    # inp = input("input = ")
+    # a = inp.split(" ")[0]
+    # b = inp.split(" ")[1]
+    # cur.execute('INSERT INTO t1(a, b) VALUES (?, ?);', (a, b))
+    cur.execute("INSERT INTO t1(order_id, status) VALUES ('{}', '{}');".format(order_id, status))
+    conn.commit()
+
+
+def delete_order(cur, b):
+    cur.execute("DELETE FROM t1 WHERE b={};".format(b))
+    conn.commit()
+
+
+def update_order(cur, b, new):
+    cur.execute("UPDATE t1 SET a = {} where b = {}".format(new, b))
+    conn.commit()
+
+
+def see_order(cur):
+    cur.execute("SELECT * FROM t1;")
+    all_results = cur.fetchall()
+    return all_results
+
+
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/all_orders", methods=['GET'])
+def index():
+    try:
+        print(see_order(cur))
+        resp = {'data': []}
+        for order in see_order(cur):
+            resp['data'].append({'order_id': order[0], 'order': order[1]})
+        print(resp)
+        return jsonify(resp)
+    except Exception:
+        return 'error'
+
+
+@app.route("/add_new_order", methods=['POST'])
+def add_new_order():
+    order_id = request.form.get('order_id')
+    status = request.form.get('status')
+    add_order(cur, order_id, status)
+    return 'ok'
+
+
+@app.route('/admin', methods=['GET'])
+def admin_page():
+    return open('admin.html', 'r', encoding='utf-8').read()
+
+
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
